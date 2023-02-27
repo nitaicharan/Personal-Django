bf89d3940b1fcb32c7bb9c688c1bd0d14c3af8e2
Nitai Charan <nitaicharan@gmail.com>
2023-02-27 00:00:00 -0500

    Writing your first Django app, part 7
    https://docs.djangoproject.com/en/4.1/intro/tutorial07/

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib import admin
 import datetime
 from django.utils import timezone
 
@@ -6,13 +7,17 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-
-    def __str__(self):
-        return self.question_text
-
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    def __str__(self):
+        return self.question_text
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
