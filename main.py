ac13743398060292d405e3ec530e1b134e44fe64
Nitai Charan <nitaicharan@gmail.com>
2023-03-11 00:00:00 -0500

    Query Parameters
    https://fastapi.tiangolo.com/tutorial/query-params

--- main.py
+++ main.py
@@ -8,29 +8,43 @@ class ModelName(str, Enum):
     resnet = "resnet"
     lenet = "lenet"
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
 @app.get("/")
 async def root():
     return { "message": "Hello Word" }
 
 @app.get("/items/{item_id}")
-async def remove_item(item_id):
-    return { "item_id": item_id }
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
 
 @app.get("/users/me")
 async def read_user_me():
     return {"user_id": "the current user"}
 
-@app.get("/users/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
 
 @app.get("/users")
 async def read_users():
     return ["Rick", "Morty"]
-
-@app.get("/users")
-async def read_users2():
-    return ["Bean", "Elfo"]
 
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
