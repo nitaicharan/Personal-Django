9998fa052acad239bcb4eedf87dedfd73d685805
Nitai Charan <nitaicharan@gmail.com>
2023-03-30 08:46:03 -0500

    Extra Data Types
    https://fastapi.tiangolo.com/tutorial/extra-data-types/

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import Body, FastAPI, Query, Path
+from datetime import datetime, time, timedelta
 from pydantic import BaseModel, Field, HttpUrl
 from typing import Annotated
 from enum import Enum
+from uuid import UUID
 
 app = FastAPI()
 
@@ -123,17 +125,25 @@ async def update_item(
     results = {"item_id": item_id, "item": item}
     return results
 
-@app.get("/items/{item_id}")
+@app.put("/items/{item_id}")
 async def read_items(
-    *,
-    item_id: Annotated[int, Path(title="The ID of the item to get", ge=0, le=1000)],
-    q: str,
-    size: Annotated[float, Query(gt=0, lt=10.5)],
+    item_id: UUID,
+    start_datetime: Annotated[datetime | None, Body()] = None,
+    end_datetime: Annotated[datetime | None, Body()] = None,
+    repeat_at: Annotated[time | None, Body()] = None,
+    process_after: Annotated[timedelta | None, Body()] = None,
 ):
-    results = {"item_id": item_id}
-    if q:
-        results.update({"q": q})
-    return results
+    start_process = start_datetime + process_after
+    duration = end_datetime - start_process
+    return {
+        "item_id": item_id,
+        "start_datetime": start_datetime,
+        "end_datetime": end_datetime,
+        "repeat_at": repeat_at,
+        "process_after": process_after,
+        "start_process": start_process,
+        "duration": duration,
+    }
 
 @app.post("/index-weights/")
 async def create_index_weights(weights: dict[int, float]):
