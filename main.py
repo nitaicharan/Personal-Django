938fe1ffc0178828c105f1d4ab0b36eb7d11cfb1
Nitai Charan <nitaicharan@gmail.com>
2023-04-03 07:21:43 -0500

    Extra Models
    https://fastapi.tiangolo.com/tutorial/extra-models/

--- main.py
+++ main.py
@@ -162,19 +162,36 @@ async def create_offer(offer: Offer):
 async def create_multiple_images(images: list[Image]):
     return images
 
-class UserIn(BaseModel):
+class UserBase(BaseModel):
     username: str
+    email: EmailStr
+    full_name: str | None = None
+
+
+class UserIn(UserBase):
     password: str
-    email: EmailStr
-    full_name: str | None = None
 
 
-class UserOut(BaseModel):
-    username: str
-    email: EmailStr
-    full_name: str | None = None
+class UserOut(UserBase):
+    pass
+
+
+class UserInDB(UserBase):
+    hashed_password: str
+
+
+def fake_password_hasher(raw_password: str):
+    return "supersecret" + raw_password
+
+
+def fake_save_user(user_in: UserIn):
+    hashed_password = fake_password_hasher(user_in.password)
+    user_in_db = UserInDB(**user_in.dict(), hashed_password=hashed_password)
+    print("User saved! ..not really")
+    return user_in_db
 
 
 @app.post("/user/", response_model=UserOut)
-async def create_user(user: UserIn) -> Any:
-    return user
+async def create_user(user_in: UserIn):
+    user_saved = fake_save_user(user_in)
+    return user_saved
