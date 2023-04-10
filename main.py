3ce5a11fcef3bee729ae89b8bbfa9f3e53ab23fa
Nitai Charan <nitaicharan@gmail.com>
2023-04-10 00:00:00 -0500

    Sub-dependencies
    https://fastapi.tiangolo.com/tutorial/dependencies/sub-dependencies/

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 from typing import Annotated
 
-from fastapi import Depends, FastAPI
+from fastapi import Cookie, Depends, FastAPI
 
 app = FastAPI()
 
 
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+def query_extractor(q: str | None = None):
+    return q
 
 
-class CommonQueryParams:
-    def __init__(self, q: str | None = None, skip: int = 0, limit: int = 100):
-        self.q = q
-        self.skip = skip
-        self.limit = limit
+def query_or_cookie_extractor(
+    q: Annotated[str, Depends(query_extractor)],
+    last_query: Annotated[str | None, Cookie()] = None,
+):
+    if not q:
+        return last_query
+    return q
 
 
 @app.get("/items/")
-async def read_items(commons: Annotated[CommonQueryParams, Depends()]):
-    response = {}
-    if commons.q:
-        response.update({"q": commons.q})
-    items = fake_items_db[commons.skip : commons.skip + commons.limit]
-    response.update({"items": items})
-    return response
+async def read_query(
+    query_or_default: Annotated[str, Depends(query_or_cookie_extractor)]
+):
+    return {"q_or_cookie": query_or_default}
+
+async def needy_dependency(fresh_value: Annotated[str, Depends(get_value, use_cache=False)]):
+    return {"fresh_value": fresh_value}
