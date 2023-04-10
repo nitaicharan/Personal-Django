5f050405aa6dc28b02f8bbcbe07685c0dcaf20a1
Nitai Charan <nitaicharan@gmail.com>
2023-04-10 00:00:00 -0500

    Dependencies in path operation decorators
    https://fastapi.tiangolo.com/tutorial/dependencies/dependencies-in-path-operation-decorators

--- main.py
+++ main.py
@@ -1,28 +1,20 @@
-from typing import Annotated
-
-from fastapi import Cookie, Depends, FastAPI
+from fastapi import Depends, FastAPI, Header, HTTPException
+from typing_extensions import Annotated
 
 app = FastAPI()
 
 
-def query_extractor(q: str | None = None):
-    return q
+async def verify_token(x_token: Annotated[str, Header()]):
+    if x_token != "fake-super-secret-token":
+        raise HTTPException(status_code=400, detail="X-Token header invalid")
 
 
-def query_or_cookie_extractor(
-    q: Annotated[str, Depends(query_extractor)],
-    last_query: Annotated[str | None, Cookie()] = None,
-):
-    if not q:
-        return last_query
-    return q
+async def verify_key(x_key: Annotated[str, Header()]):
+    if x_key != "fake-super-secret-key":
+        raise HTTPException(status_code=400, detail="X-Key header invalid")
+    return x_key
 
 
-@app.get("/items/")
-async def read_query(
-    query_or_default: Annotated[str, Depends(query_or_cookie_extractor)]
-):
-    return {"q_or_cookie": query_or_default}
-
-async def needy_dependency(fresh_value: Annotated[str, Depends(get_value, use_cache=False)]):
-    return {"fresh_value": fresh_value}
+@app.get("/items/", dependencies=[Depends(verify_token), Depends(verify_key)])
+async def read_items():
+    return [{"item": "Foo"}, {"item": "Bar"}]
