0030f374d903fca25d1e18d21098d15e9d9a8988
Nitai Charan <nitaicharan@gmail.com>
2023-04-10 07:08:14 -0500

    Request Files
    https://fastapi.tiangolo.com/tutorial/request-files/

--- main.py
+++ main.py
@@ -199,3 +199,11 @@ async def create_user(user_in: UserIn):
 @app.post("/login/")
 async def login(username: Annotated[str, Form()], password: Annotated[str, Form()]):
     return {"username": username}
+
+@app.post("/files/")
+async def create_files(files: Annotated[list[bytes], File()]):
+    return {"file_sizes": [len(file) for file in files]}
+
+@app.post("/uploadfiles/")
+async def create_upload_files(files: list[UploadFile]):
+    return {"filenames": [file.filename for file in files]}
