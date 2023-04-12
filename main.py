e50134c1c53cc0007ffd2fc0e990db1282711089
Nitai Charan <nitaicharan@gmail.com>
2023-04-12 08:46:14 -0500

    Dependencies with yield
    https://fastapi.tiangolo.com/tutorial/dependencies/dependencies-with-yield/

--- main.py
+++ main.py
@@ -1,26 +1,42 @@
-from fastapi import Depends, FastAPI, Header, HTTPException
-from typing_extensions import Annotated
+from typing import Annotated
+
+from fastapi import Depends
 
 
-async def verify_token(x_token: Annotated[str, Header()]):
-    if x_token != "fake-super-secret-token":
-        raise HTTPException(status_code=400, detail="X-Token header invalid")
+async def dependency_a():
+    dep_a = generate_dep_a()
+    try:
+        yield dep_a
+    finally:
+        dep_a.close()
 
 
-async def verify_key(x_key: Annotated[str, Header()]):
-    if x_key != "fake-super-secret-key":
-        raise HTTPException(status_code=400, detail="X-Key header invalid")
-    return x_key
+async def dependency_b(dep_a: Annotated[DepA, Depends(dependency_a)]):
+    dep_b = generate_dep_b()
+    try:
+        yield dep_b
+    finally:
+        dep_b.close(dep_a)
 
 
-app = FastAPI(dependencies=[Depends(verify_token), Depends(verify_key)])
+async def dependency_c(dep_b: Annotated[DepB, Depends(dependency_b)]):
+    dep_c = generate_dep_c()
+    try:
+        yield dep_c
+    finally:
+        dep_c.close(dep_b)
+
+class MySuperContextManager:
+    def __init__(self):
+        self.db = DBSession()
+
+    def __enter__(self):
+        return self.db
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.db.close()
 
 
-@app.get("/items/")
-async def read_items():
-    return [{"item": "Portal Gun"}, {"item": "Plumbus"}]
-
-
-@app.get("/users/")
-async def read_users():
-    return [{"username": "Rick"}, {"username": "Morty"}]
+async def get_db():
+    with MySuperContextManager() as db:
+        yield db
