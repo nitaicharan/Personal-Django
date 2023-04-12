c07391b6624a365f43fc37c9dae80e8546d72e92
Nitai Charan <nitaicharan@gmail.com>
2023-04-12 09:00:38 -0500

    Security - First Steps
    https://fastapi.tiangolo.com/tutorial/security/first-steps/

--- main.py
+++ main.py
@@ -1,42 +1,13 @@
 from typing import Annotated
 
-from fastapi import Depends
+from fastapi import Depends, FastAPI
+from fastapi.security import OAuth2PasswordBearer
+
+app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
-async def dependency_a():
-    dep_a = generate_dep_a()
-    try:
-        yield dep_a
-    finally:
-        dep_a.close()
-
-
-async def dependency_b(dep_a: Annotated[DepA, Depends(dependency_a)]):
-    dep_b = generate_dep_b()
-    try:
-        yield dep_b
-    finally:
-        dep_b.close(dep_a)
-
-
-async def dependency_c(dep_b: Annotated[DepB, Depends(dependency_b)]):
-    dep_c = generate_dep_c()
-    try:
-        yield dep_c
-    finally:
-        dep_c.close(dep_b)
-
-class MySuperContextManager:
-    def __init__(self):
-        self.db = DBSession()
-
-    def __enter__(self):
-        return self.db
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.db.close()
-
-
-async def get_db():
-    with MySuperContextManager() as db:
-        yield db
+@app.get("/items/")
+async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
+    return {"token": token}
