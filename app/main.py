d296b200851bbd0857a1e561164c0744addb8b4c
Nitai Charan <nitaicharan@gmail.com>
2024-07-23 19:41:40 +0100

    chore(dev): create `start` script to run project with `pyproject`

--- app/main.py
+++ app/main.py
@@ -1,3 +1,12 @@
+import uvicorn
 from app.src.infrastructure.app import create_app
 
 app = create_app()
+
+
+def main():
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+if __name__ == "__main__":
+    main()
